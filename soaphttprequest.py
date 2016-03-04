9a7a24a5e1d98d4c22c24df67ee616bf6a4d5e92
a455bcd9 <kelemonde.aibo@club-internet.fr>
2016-03-04 15:42:05 +0100

    new methods

---
+++ soaphttprequest.py
@@ -0,0 +1,33 @@
+import requests
+url="http://eur-lex.europa.eu/eurlex-ws?wsdl"
+# headers = {'content-type': 'application/soap+xml'}
+# Content-Type: application/soap+xml; charset=utf-8
+headers = {'content-type': 'application/soap+xml', 'charset': 'utf-8'}
+# headers = {'content-type': 'text/plain'}
+# headers = {'content-type': 'text/xml'}
+body = """<?xml version="1.0" encoding="UTF-8"?>
+<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:sear="http://eur-lex.europa.eu/search">
+<soap:Header>
+<wsse:Security xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" soap:mustUnderstand="true">
+<wsse:UsernameToken xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd" wsu:Id="UsernameToken-1">
+<wsse:Username></wsse:Username>
+<wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">password</wsse:Password>
+</wsse:UsernameToken>
+</wsse:Security>
+</soap:Header>
+<!-- http://eur-lex.europa.eu/eurlex-ws?wsdl -->
+<soap:Body>
+<sear:searchRequest>
+<sear:expertQuery>DTS_SUBDOM = EU_CASE_LAW</sear:expertQuery>
+<sear:page>1</sear:page>
+<!-- Max 100 -->
+<sear:pageSize>1</sear:pageSize>
+<sear:searchLanguage>fr</sear:searchLanguage>
+</sear:searchRequest>
+</soap:Body>
+</soap:Envelope>"""
+
+response = requests.get(url,data=body,headers=headers)
+# print response.json()
+print response.content
+# print response.encoding
